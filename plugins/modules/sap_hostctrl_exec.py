967215ef51701b79c5cd830359ca202cf93c0609
Marcel Mamula <marcel.mamula@suse.com>
2026-03-13 09:54:55 +0100

    update license header to correct license

--- plugins/modules/sap_hostctrl_exec.py
+++ plugins/modules/sap_hostctrl_exec.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python
 
-# Copyright: (c) 2022, Rainer Leber rainerleber@gmail.com, rainer.leber@sva.de,
-#                      Robert Kraemer @rkpobe, robert.kraemer@sva.de,
-#                      Yannick Douvry, ydouvry@oxya.com
+# Copyright (c) 2022-2026 The Project Contributors.
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# For a detailed list of copyright holders and contribution history,
+# please refer to the CONTRIBUTORS.md file in the project root.
+
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
